553b18b1b4e9dee3882a6e9f4331e0a44c8094b5
Brent Johnson <brent.programmer@gmail.com>
2012-11-28 16:29:27 -0800

    Implemented more functions for Application and Item services.

---
+++ Podio.API/Utils/ItemReference.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Podio.API.Model
+{
+    [DataContract]
+    public partial class ItemReference
+    {
+        [DataMember(Name = "field", IsRequired = false)]
+        public ItemField Field { get; set; }
+
+        [DataMember(Name = "app", IsRequired = false)]
+        public Application App { get; set; }
+
+        [DataMember(Name = "items", IsRequired = false)]
+        public List<Item> Items { get; set; }
+    }
+}
